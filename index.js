2edc3465d5b95c022f03a8182d37ed94a1b27105
Elisa Situ Huang <elisasituhuang@gmail.com>
2022-10-18 13:44:14 -0400

    commit tree 9dcc6b9467ef2c5385cc38798e0eaacb375e1101

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData(userNames, userEmails) {
+    const userData = {
+        name: userNames,
+        email: userEmails,
+    }
+        return fetch('http://localhost:3000/users',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+          body: JSON.stringify(userData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.body.innerHTML = data.id
+        })
+        .catch(error => {
+          let message = 'Unauthorized Access';
+          document.body.innerHTML = error;
+       })
+      }
+    
+    
+    
+    
+    
+    
+    
+    
